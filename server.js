1cd728b9e0a282f9f09be5e84acb0a14f8e51b28
Tosffy <tosffy@gmail.com>
2025-07-24 16:05:37 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3123;
 
 app.get('/', (req, res) => {
   res.send('Short Video Maker Server is running!');
